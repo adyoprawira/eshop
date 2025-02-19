58415327f66d2fc2c5e7bf6b8c89a18f7a8286b8
adyoprawira <adyoprawira@gmail.com>
2025-02-19 12:08:21 +0700

    refactor: removed unused imports

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,11 +1,8 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class EshopApplicationTests {
