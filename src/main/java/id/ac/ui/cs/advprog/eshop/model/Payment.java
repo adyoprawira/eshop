3bb41dbc79352f56fc70f4107c75985517858e4e
adyoprawira <adyoprawira@gmail.com>
2025-03-07 17:48:45 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -16,14 +16,41 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.setMethod(method); // Use setter for validation
+        this.paymentData = paymentData;
+        this.setStatus(status); // Use setter for validation
     }
 
     public void setStatus(String status) {
+        if (status == null || status.isEmpty()) {
+            this.status = status;
+            return;
+        }
+
+        if (!status.equals("SUCCESS") && !status.equals("PENDING") && !status.equals("REJECTED")) {
+            throw new IllegalArgumentException("Invalid payment status: " + status);
+        }
+        this.status = status;
     }
 
     public void setMethod(String method) {
+        if (method == null || method.isEmpty()) {
+            this.method = method;
+            return;
+        }
+
+        if (!method.equals("VOUCHER") && !method.equals("BANK_TRANSFER")) {
+            throw new IllegalArgumentException("Invalid payment method: " + method);
+        }
+        this.method = method;
     }
 
     public void setId(String id) {
+        if (id == null || id.isEmpty()) {
+            this.id = id;
+            return;
+        }
+        this.id = id;
     }
 }
