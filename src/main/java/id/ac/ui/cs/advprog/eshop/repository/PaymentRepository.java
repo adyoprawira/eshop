39f8cec8dfc5f05cc0806e0ed1683f6e2df8d4b0
adyoprawira <adyoprawira@gmail.com>
2025-03-07 19:41:45 +0700

    [RED] Added PaymentRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.Iterator;
+
+public class PaymentRepository {
+
+    public Payment create(Payment payment) {
+        return null;
+    }
+
+    public Iterator<Payment> findAll() {
+        return null;
+    }
+
+    public Payment findById(String paymentId) {
+        return null;
+    }
+
+    public Payment update(Payment payment) {
+        return null;
+    }
+}
